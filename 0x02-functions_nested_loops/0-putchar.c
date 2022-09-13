ea06e58fe9dd60cc97d482557c0cc1c7d11f777c
Ebenje Emmanuel <emmanuelebenje@gmail.com>
2022-09-13 13:13:35 -0600

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,27 +9,6 @@
  * */
 int main(void)
 {
-		putchar('');
-			_putchar('p');
-				_putchar('u');
-					_putchar('t');
-						_putchar('c');
-							_putchar('h');
-								_putchar('a');
-									_putchar('r');
-										_putchar('\n');
-											return (0);
-#include "main.h"
-
-/**
- * * main - prints Putchar
- * *
- * *Description: prints _putchar
- * *
- * *Return: 0 (Success)
- * */
-int main(void)
-{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
